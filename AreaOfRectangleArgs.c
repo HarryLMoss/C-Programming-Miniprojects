bd3c0bf9875f155f2229f017944394879612a592
Harry Moss <harrymoss33@gmail.com>
2024-05-08 15:20:18 +0100

    Update AreaOfRectangleArgs.c

--- AreaOfRectangleArgs.c
+++ AreaOfRectangleArgs.c
@@ -4,9 +4,9 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(int argc, char *argv[])
+int main(int argc, char *argv[])        // main function using command line arguments
 {
-    double width = atoi(argv[1]);
+    double width = atoi(argv[1]);       //convert command line strings into integers
     double height = atoi(argv[2]);
 
     double perimeter = 0.0;
@@ -15,7 +15,7 @@ int main(int argc, char *argv[])
     perimeter = 2.0 * (height + width);
     area = width * height;
 
-    printf("The perimeter is %.2f\n", perimeter);
+    printf("The perimeter is %.2f\n", perimeter);    // .2 is to 2 decimal points
     printf("The area is %.2f\n", area);
 
 
