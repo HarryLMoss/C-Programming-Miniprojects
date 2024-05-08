f6f6eea2a55de9c837bdb93ac54f1d3bcc916387
Harry Moss <harrymoss33@gmail.com>
2024-05-08 15:22:10 +0100

    Update ArgumentExample.c

--- ArgumentExample.c
+++ ArgumentExample.c
@@ -6,9 +6,9 @@
 
 int main(int argc, char *argv[])
 {
-    int numberOfArguments = argc;
-    char *argument1 = argv[0];
-    char *argument2 = argv[1];
+    int numberOfArguments = argc;             // argc is the number of command line arguments including the program name
+    char *argument1 = argv[0];                // argv[0] is always the program name
+    char *argument2 = argv[1];                // argv[1] is the first command line argument
 
     printf("Number of Arguments: %d\n", numberOfArguments);
     printf("Argument 1 is the program name: %s\n", argument1);
