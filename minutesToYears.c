0fe9de1b099c16f920b093ab721dba665d04f430
Harry Moss <harrymoss33@gmail.com>
2024-05-08 15:02:20 +0100

    Added C Miniproject

---
+++ minutesToYears.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+int main()
+{
+    double minutes = 0.0;
+    double minutesInYear = 0.0;
+    double years = 0.0;
+    int days = 0.0;
+
+    printf("Enter the number of minutes into the terminal: ");
+    scanf("%lf", &minutes);
+
+    minutesInYear = (60 * 24 * 365);
+    years = (minutes / minutesInYear);
+    days = (int) (years * 365);
+
+    printf("Years: %f\n", years);
+    printf("Days: %d", days);
+
+    return 0;
+}
