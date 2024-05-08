6122c0a1deba59a48956ad1a8779016726d2b0a8
Harry Moss <harrymoss33@gmail.com>
2024-05-08 15:06:38 +0100

    Update fileLineCounter.c

--- fileLineCounter.c
+++ fileLineCounter.c
@@ -1,21 +1,23 @@
+// Author: Harry Moss
+// Date: 08.05.2024
+
 #include <stdio.h>
 #include <stdlib.h>
 #include <string.h>
 
-
 int main(void)
 {
-    FILE *fp = NULL;
+    FILE *fp = NULL;                //set file pointer to be NULL
     int c, count = 0;
 
-    fp = fopen("text.txt", "r"); // replace filename.txt with your file name
+    fp = fopen("text.txt", "r");    // replace text.txt with your file name
 
     if (fp == NULL) {
-        printf("Error opening.\n");
+        printf("Error opening.\n"); // return error -1 if pointer still NULL
         return -1;
     }
 
-    while((c = fgetc(fp)) != EOF)
+    while((c = fgetc(fp)) != EOF)   // while loop until End Of File is reached
     {
         if(c == '\n')
             count++;
